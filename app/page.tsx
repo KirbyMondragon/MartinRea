82b2ca43249512af0a3e7515d94554d3e5b4db2e
Kevin Mondragon <021000615@upsrj.edu.mx>
2025-03-05 10:03:02 -0600

    Change names

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ import { buttonVariants } from "@/components/ui/button"
 export default function Home() {
   return (
     <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
-      <h1 className="text-4xl font-bold mb-4 sm:text-7xl">Documents</h1>
+      <h1 className="text-4xl font-bold mb-4 sm:text-7xl">Martinrea</h1>
+      <h2 className="text-4xl font-bold mb-4 sm:text-7xl">(Documentación de máquinas)</h2>
       <p className="max-w-[600px] text-foreground mb-8 sm:text-base">
-        A simple open-source product documentation platform. That&apos;s simple
-        to use and easy to customize.
+    Portal confiable para acceder a manuales y guías técnicas de máquinas. Con instrucciones paso a paso y recursos actualizados, la página te ayuda a mantener, reparar y optimizar tus equipos de manera sencilla y eficiente.
       </p>
       <div className="flex items-center gap-5">
         <Link
